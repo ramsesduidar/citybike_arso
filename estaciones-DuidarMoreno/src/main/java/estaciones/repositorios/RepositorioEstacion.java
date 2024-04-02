daa59442e6c549931ad63d1010c1757c8665e85d
Piero Alexander <pierokinston2@gmail.com>
2024-04-02 16:14:58 +0200

    Avance-Implementacion con Spring Boot

--- estaciones-DuidarMoreno/src/main/java/estaciones/repositorios/RepositorioEstacion.java
+++ estaciones-DuidarMoreno/src/main/java/estaciones/repositorios/RepositorioEstacion.java
@@ -4,24 +4,30 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.geo.Point;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.NoRepositoryBean;
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import estaciones.dominio.Estacion;
-import repositorios.RepositorioException;
+//import repositorios.RepositorioException;
 
-@NoRepositoryBean
-public interface RepositorioEstacion 
+@Repository
+public interface RepositorioEstacion  extends PagingAndSortingRepository<Estacion, String> {
 	//extends PagingAndSortingRepository<Estacion, String>{
-	extends CrudRepository<Estacion, String>{
+	//extends CrudRepository<Estacion, String>{
 	
-	List<Estacion> findFirstLibre();
+    List<Estacion> findFirstByDisponibleTrue();
+
+	//List<Estacion> findFirstLibre();
 	
 	Estacion findFirstByNombre(String titulo);
 
 	List<Estacion> findFirst3ByCoordenadasNear(Point coordenadas);
 
-	List<Estacion> getEstacionesPorSitios();
+    List<Estacion> findBySitiosTuristicos(List<String> idsSitios);
+
+	//List<Estacion> getEstacionesPorSitios();
 	
 }
